cc033e68034fb9967f31a528af2a0c46d7658a32
bibliothec <mupimupicandy@gmail.com>
2021-08-18 18:05:52 +0900

    move title to play

---
+++ Assets/script/PressStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressStart : MonoBehaviour
+{
+    private bool firstPush=false;
+
+    public void OnClickPressStart()
+    {
+        Debug.Log("Press Start!");
+        if(!firstPush)
+        {
+            Debug.Log("Go Next Scene!");
+            //write command to move next scene
+            SceneManager.LoadScene("play");
+            firstPush=true;
+        }
+    }
+}
