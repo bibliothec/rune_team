1fc78638144803931ae12a8d1b5d6987c9208805
bibliothec <mupimupicandy@gmail.com>
2021-08-18 19:04:14 +0900

    make test animation

---
+++ Assets/test/animation/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator anim=null;
+    void Start()
+    {
+        anim=GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float horizontalKey=Input.GetAxis("Horizontal");
+        if(horizontalKey>0)
+        {
+            transform.localScale=new Vector3(1,1,1);
+            anim.SetBool("run",true);
+        }else if(horizontalKey<0)
+        {
+            transform.localScale=new Vector3(-1,1,1);
+            anim.SetBool("run",true);
+        }else
+        {
+            anim.SetBool("run",false);
+        }
+    }
+}
