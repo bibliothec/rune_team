7c1b5e97bd3460e690518b4c1d659f91d254103a
bibliothec <mupimupicandy@gmail.com>
2021-08-18 19:59:05 +0900

    make move

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+
     private Animator anim=null;
+    private Rigidbody2D rb=null;
     void Start()
     {
         anim=GetComponent<Animator>();
+        rb=GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         float horizontalKey=Input.GetAxis("Horizontal");
+        float xSpeed=0.0f;
         if(horizontalKey>0)
         {
             transform.localScale=new Vector3(1,1,1);
             anim.SetBool("run",true);
+            xSpeed=speed;
         }else if(horizontalKey<0)
         {
             transform.localScale=new Vector3(-1,1,1);
             anim.SetBool("run",true);
+            xSpeed=-speed;
         }else
         {
             anim.SetBool("run",false);
+            xSpeed=0.0f;
         }
+        rb.velocity=new Vector2(xSpeed,rb.velocity.y);
     }
 }
