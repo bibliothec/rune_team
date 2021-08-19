81aaa4954d4be091bb028a20d0640fe5effce861
shinki <bb38120078@ms.nagasaki-u.ac.jp>
2021-08-19 14:11:02 +0900

    jump

---
+++ Assets/script/bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+    //生成するゲームオブジェクト
+    public GameObject bullet1;
+    public float bulletSpeed = 1f;
+    void Update()
+    {
+    if (Input.GetKeyDown("enter"))
+        {
+            GameObject runcherBullet = GameObject.Instantiate(bullet1) as GameObject; //runcherbulletにbulletのインスタンスを格納
+            runcherBullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed; //アタッチしているオブジェクトの前方にbullet speedの速さで発射
+            runcherBullet.transform.position = transform.position;
+        }
+    }
+}
